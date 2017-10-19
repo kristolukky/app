ef3b821471422fa43737431676a825bda0bac27e
kristolukky <kristolukky@gmail.com>
2017-10-19 17:11:09 +0300

    app 1.0

--- warehouses.sql
+++ warehouses.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:3333
--- Время создания: Окт 09 2017 г., 13:17
+-- Время создания: Окт 19 2017 г., 17:08
 -- Версия сервера: 5.7.13-log
 -- Версия PHP: 7.0.8
 
@@ -30,9 +30,8 @@ CREATE TABLE IF NOT EXISTS `warehouses` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `WH1` int(11) DEFAULT NULL,
-  `WH2` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+  `WH` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Индексы сохранённых таблиц
@@ -52,7 +51,7 @@ ALTER TABLE `warehouses`
 -- AUTO_INCREMENT для таблицы `warehouses`
 --
 ALTER TABLE `warehouses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
